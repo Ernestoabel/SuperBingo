dea776d4a912175fed5e798a017ce41aeb8cb986
Ernestoabel <ernestoabel83@gmail.com>
2023-11-06 11:25:50 -0300

    Agregar archivos de proyecto.

---
+++ Bingo/FrmBolillero.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
+namespace Bingo2E
+{
+
+    public partial class FrmBolillero : Form
+    {
+        static Random rnd;
+        List<int> numerosDisponibles;
+        List<int> numerosCantados;
+        List<FrmCarton> cartones = new List<FrmCarton>();
+        public delegate void MiDelegado(int numero);
+        public MiDelegado fm;
+
+        public FrmBolillero()
+        {
+            InitializeComponent();
+            rnd = new Random();
+            numerosDisponibles = new List<int>();
+            numerosCantados = new List<int>();
+            SetearEstadoInicial();
+        }
+        /*
+        public void agregarFuncion (MiDelegado func)
+        {
+            fm += func;
+        } 
+        */
+        private void SetearEstadoInicial()
+        {
+            numerosCantados.Clear();
+
+            for (int i = 0; i < 100; i++)
+            {
+                numerosDisponibles.Add(i);
+            }
+        }
+
+        private int ObtenerNumero()
+        {
+            int numeroQueSalio = rnd.Next(0, numerosDisponibles.Count);
+
+            numeroQueSalio = numerosDisponibles[numeroQueSalio];
+
+            numerosDisponibles.Remove(numeroQueSalio);
+
+            numerosCantados.Add(numeroQueSalio);
+
+            return numeroQueSalio;
+        }
+
+        private void btn_numero_Click(object sender, EventArgs e)
+        {
+            
+            int numero = ObtenerNumero();
+            label2.Text = "Ultimo Numero:" + numero.ToString();
+            numerosCantados = numerosCantados.OrderBy(n => n).ToList();
+            listBox1.Items.Clear();
+            foreach (int num in numerosCantados)
+            {
+                listBox1.Items.Add(num);
+            }
+            //fm(numero);
+            
+            foreach (var carton in cartones)
+            {
+                carton.fm(numero);
+                int numerosPendientes = carton.CantidadNumerosPendientes();
+                if (numerosPendientes == 0)
+                {
+                    CantarBingo(carton.nombreJugador);
+                }
+            }
+
+        }
+
+        public void CantarBingo(string nombre)
+        {
+            MessageBox.Show("Ganador!!! : " + nombre);
+
+            this.btn_numero.Enabled = false;
+
+        }
+
+
+        private void FrmBolillero_Load(object sender, EventArgs e)
+        {
+            /*
+            new FrmCarton("Pepe").Show();
+            new FrmCarton("Juana").Show();
+            new FrmCarton("Caro").Show();
+            */
+            FrmCarton carton1 = new FrmCarton("Pepe");
+            FrmCarton carton2 = new FrmCarton("Juana");
+            FrmCarton carton3 = new FrmCarton("Caro");
+
+            cartones.Add(carton1);
+            cartones.Add(carton2);
+            cartones.Add(carton3);
+
+            foreach (var carton in cartones)
+            {
+                carton.Show();
+            }
+        }
+    }
+}
---
+++ Bingo/FrmCarton.Designer.cs
@@ -0,0 +1,285 @@
+﻿
+namespace Bingo2E
+{
+    partial class FrmCarton
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pb_carton = new System.Windows.Forms.PictureBox();
+            lbl_num1 = new System.Windows.Forms.Label();
+            lbl_num2 = new System.Windows.Forms.Label();
+            lbl_num3 = new System.Windows.Forms.Label();
+            lbl_num4 = new System.Windows.Forms.Label();
+            lbl_num5 = new System.Windows.Forms.Label();
+            lbl_num6 = new System.Windows.Forms.Label();
+            lbl_num7 = new System.Windows.Forms.Label();
+            lbl_num8 = new System.Windows.Forms.Label();
+            lbl_num10 = new System.Windows.Forms.Label();
+            lbl_num9 = new System.Windows.Forms.Label();
+            lbl_num11 = new System.Windows.Forms.Label();
+            lbl_num12 = new System.Windows.Forms.Label();
+            lbl_num13 = new System.Windows.Forms.Label();
+            lbl_num14 = new System.Windows.Forms.Label();
+            lbl_num15 = new System.Windows.Forms.Label();
+            lbl_ultimoNum = new System.Windows.Forms.Label();
+            lb_jugador = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)pb_carton).BeginInit();
+            SuspendLayout();
+            // 
+            // pb_carton
+            // 
+            pb_carton.Image = Bingo.Properties.Resources.carton;
+            pb_carton.Location = new System.Drawing.Point(12, 73);
+            pb_carton.Name = "pb_carton";
+            pb_carton.Size = new System.Drawing.Size(481, 197);
+            pb_carton.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            pb_carton.TabIndex = 0;
+            pb_carton.TabStop = false;
+            // 
+            // lbl_num1
+            // 
+            lbl_num1.AutoSize = true;
+            lbl_num1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            lbl_num1.Location = new System.Drawing.Point(53, 108);
+            lbl_num1.Name = "lbl_num1";
+            lbl_num1.Size = new System.Drawing.Size(28, 21);
+            lbl_num1.TabIndex = 1;
+            lbl_num1.Text = "00";
+            // 
+            // lbl_num2
+            // 
+            lbl_num2.AutoSize = true;
+            lbl_num2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            lbl_num2.Location = new System.Drawing.Point(147, 108);
+            lbl_num2.Name = "lbl_num2";
+            lbl_num2.Size = new System.Drawing.Size(28, 21);
+            lbl_num2.TabIndex = 2;
+            lbl_num2.Text = "22";
+            // 
+            // lbl_num3
+            // 
+            lbl_num3.AutoSize = true;
+            lbl_num3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            lbl_num3.Location = new System.Drawing.Point(240, 108);
+            lbl_num3.Name = "lbl_num3";
+            lbl_num3.Size = new System.Drawing.Size(28, 21);
+            lbl_num3.TabIndex = 3;
+            lbl_num3.Text = "22";
+            // 
+            // lbl_num4
+            // 
+            lbl_num4.AutoSize = true;
+            lbl_num4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            lbl_num4.Location = new System.Drawing.Point(330, 107);
+            lbl_num4.Name = "lbl_num4";
+            lbl_num4.Size = new System.Drawing.Size(28, 21);
+            lbl_num4.TabIndex = 4;
+            lbl_num4.Text = "22";
+            // 
+            // lbl_num5
+            // 
+            lbl_num5.AutoSize = true;
+            lbl_num5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            lbl_num5.Location = new System.Drawing.Point(424, 107);
+            lbl_num5.Name = "lbl_num5";
+            lbl_num5.Size = new System.Drawing.Size(28, 21);
+            lbl_num5.TabIndex = 5;
+            lbl_num5.Text = "22";
+            // 
+            // lbl_num6
+            // 
+            lbl_num6.AutoSize = true;
+            lbl_num6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            lbl_num6.Location = new System.Drawing.Point(100, 163);
+            lbl_num6.Name = "lbl_num6";
+            lbl_num6.Size = new System.Drawing.Size(28, 21);
+            lbl_num6.TabIndex = 6;
+            lbl_num6.Text = "22";
+            // 
+            // lbl_num7
+            // 
+            lbl_num7.AutoSize = true;
+            lbl_num7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            lbl_num7.Location = new System.Drawing.Point(193, 163);
+            lbl_num7.Name = "lbl_num7";
+            lbl_num7.Size = new System.Drawing.Size(28, 21);
+            lbl_num7.TabIndex = 7;
+            lbl_num7.Text = "22";
+            // 
+            // lbl_num8
+            // 
+            lbl_num8.AutoSize = true;
+            lbl_num8.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            lbl_num8.Location = new System.Drawing.Point(238, 163);
+            lbl_num8.Name = "lbl_num8";
+            lbl_num8.Size = new System.Drawing.Size(28, 21);
+            lbl_num8.TabIndex = 8;
+            lbl_num8.Text = "22";
+            // 
+            // lbl_num10
+            // 
+            lbl_num10.AutoSize = true;
+            lbl_num10.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            lbl_num10.Location = new System.Drawing.Point(378, 163);
+            lbl_num10.Name = "lbl_num10";
+            lbl_num10.Size = new System.Drawing.Size(28, 21);
+            lbl_num10.TabIndex = 9;
+            lbl_num10.Text = "22";
+            // 
+            // lbl_num9
+            // 
+            lbl_num9.AutoSize = true;
+            lbl_num9.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            lbl_num9.Location = new System.Drawing.Point(285, 163);
+            lbl_num9.Name = "lbl_num9";
+            lbl_num9.Size = new System.Drawing.Size(28, 21);
+            lbl_num9.TabIndex = 10;
+            lbl_num9.Text = "22";
+            // 
+            // lbl_num11
+            // 
+            lbl_num11.AutoSize = true;
+            lbl_num11.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            lbl_num11.Location = new System.Drawing.Point(53, 217);
+            lbl_num11.Name = "lbl_num11";
+            lbl_num11.Size = new System.Drawing.Size(28, 21);
+            lbl_num11.TabIndex = 11;
+            lbl_num11.Text = "22";
+            // 
+            // lbl_num12
+            // 
+            lbl_num12.AutoSize = true;
+            lbl_num12.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            lbl_num12.Location = new System.Drawing.Point(146, 217);
+            lbl_num12.Name = "lbl_num12";
+            lbl_num12.Size = new System.Drawing.Size(28, 21);
+            lbl_num12.TabIndex = 13;
+            lbl_num12.Text = "22";
+            // 
+            // lbl_num13
+            // 
+            lbl_num13.AutoSize = true;
+            lbl_num13.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            lbl_num13.Location = new System.Drawing.Point(192, 217);
+            lbl_num13.Name = "lbl_num13";
+            lbl_num13.Size = new System.Drawing.Size(28, 21);
+            lbl_num13.TabIndex = 14;
+            lbl_num13.Text = "22";
+            // 
+            // lbl_num14
+            // 
+            lbl_num14.AutoSize = true;
+            lbl_num14.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            lbl_num14.Location = new System.Drawing.Point(333, 217);
+            lbl_num14.Name = "lbl_num14";
+            lbl_num14.Size = new System.Drawing.Size(28, 21);
+            lbl_num14.TabIndex = 15;
+            lbl_num14.Text = "22";
+            // 
+            // lbl_num15
+            // 
+            lbl_num15.AutoSize = true;
+            lbl_num15.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            lbl_num15.Location = new System.Drawing.Point(425, 217);
+            lbl_num15.Name = "lbl_num15";
+            lbl_num15.Size = new System.Drawing.Size(28, 21);
+            lbl_num15.TabIndex = 16;
+            lbl_num15.Text = "22";
+            // 
+            // lbl_ultimoNum
+            // 
+            lbl_ultimoNum.AutoSize = true;
+            lbl_ultimoNum.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            lbl_ultimoNum.Location = new System.Drawing.Point(206, 291);
+            lbl_ultimoNum.Name = "lbl_ultimoNum";
+            lbl_ultimoNum.Size = new System.Drawing.Size(297, 54);
+            lbl_ultimoNum.TabIndex = 17;
+            lbl_ultimoNum.Text = "Ultimo Numero";
+            // 
+            // lb_jugador
+            // 
+            lb_jugador.AutoSize = true;
+            lb_jugador.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            lb_jugador.Location = new System.Drawing.Point(10, 7);
+            lb_jugador.Name = "lb_jugador";
+            lb_jugador.Size = new System.Drawing.Size(165, 54);
+            lb_jugador.TabIndex = 18;
+            lb_jugador.Text = "Jugador";
+            // 
+            // FrmCarton
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(522, 350);
+            Controls.Add(lb_jugador);
+            Controls.Add(lbl_ultimoNum);
+            Controls.Add(lbl_num15);
+            Controls.Add(lbl_num14);
+            Controls.Add(lbl_num13);
+            Controls.Add(lbl_num12);
+            Controls.Add(lbl_num11);
+            Controls.Add(lbl_num9);
+            Controls.Add(lbl_num10);
+            Controls.Add(lbl_num8);
+            Controls.Add(lbl_num7);
+            Controls.Add(lbl_num6);
+            Controls.Add(lbl_num5);
+            Controls.Add(lbl_num4);
+            Controls.Add(lbl_num3);
+            Controls.Add(lbl_num2);
+            Controls.Add(lbl_num1);
+            Controls.Add(pb_carton);
+            Name = "FrmCarton";
+            Text = "Carton";
+            Load += FrmCarton_Load;
+            ((System.ComponentModel.ISupportInitialize)pb_carton).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pb_carton;
+        private System.Windows.Forms.Label lbl_num1;
+        private System.Windows.Forms.Label lbl_num2;
+        private System.Windows.Forms.Label lbl_num3;
+        private System.Windows.Forms.Label lbl_num4;
+        private System.Windows.Forms.Label lbl_num5;
+        private System.Windows.Forms.Label lbl_num6;
+        private System.Windows.Forms.Label lbl_num7;
+        private System.Windows.Forms.Label lbl_num8;
+        private System.Windows.Forms.Label lbl_num10;
+        private System.Windows.Forms.Label lbl_num9;
+        private System.Windows.Forms.Label lbl_num11;
+        private System.Windows.Forms.Label lbl_num12;
+        private System.Windows.Forms.Label lbl_num13;
+        private System.Windows.Forms.Label lbl_num14;
+        private System.Windows.Forms.Label lbl_num15;
+        private System.Windows.Forms.Label lbl_ultimoNum;
+        private System.Windows.Forms.Label lb_jugador;
+    }
+}
